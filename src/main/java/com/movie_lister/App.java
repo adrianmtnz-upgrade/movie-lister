5aaca34a0a0d8f708427165ccf6efbf5c7794b41
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2025-03-23 16:06:07 +0100

    Initial commit

---
+++ src/main/java/com/movie_lister/App.java
@@ -0,0 +1,13 @@
+package com.movie_lister;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
