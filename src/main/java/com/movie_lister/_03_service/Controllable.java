99eff63dc15b4bae0e11c4d4e7ffb150844dbb72
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2025-03-27 19:29:43 +0100

    Controllable interface

--- src/main/java/com/movie_lister/_03_service/Controllable.java
+++ src/main/java/com/movie_lister/_03_service/Controllable.java
@@ -1,5 +1,10 @@
 package com.movie_lister._03_service;
 
-public interface Controllable {
 
+public interface Controllable {
+    public void initiateController();
+    public void listMovies();
+    public void addMovie(String title);
+    public void deleteMovie(String title);
+    public void searchMovie(String movieToSearch);
 }
--- src/main/java/com/movie_lister/_03_service/MovieCatalogController.java
+++ src/main/java/com/movie_lister/_03_service/MovieCatalogController.java
@@ -2,4 +2,34 @@ package com.movie_lister._03_service;
 
 public class MovieCatalogController implements Controllable {
 
+    @Override
+    public void initiateController() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initiateController'");
+    }
+
+    @Override
+    public void listMovies() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listMovies'");
+    }
+
+    @Override
+    public void addMovie(String title) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addMovie'");
+    }
+
+    @Override
+    public void deleteMovie(String title) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteMovie'");
+    }
+
+    @Override
+    public void searchMovie(String movieToSearch) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchMovie'");
+    }
+
 }
